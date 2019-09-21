5c4690ba94d8a8decdf96bb510fbe7eefa316fb7
909jfguerrero <jfguerrero@csu.fullerton.edu>
2019-09-21 14:26:57 -0700

    did prob03

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,31 @@
 // Random Rectangle
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main ()
+{
+  //set the maximum base of the rectangle to 40
+  int max = 40;
+  int base;
+
+  //generate random length
+  unsigned seed = time (0);
+  srand (seed);
+  base = rand () % max + 1;
+
+  // show Rectangle
+  std::string rectangle;
+  rectangle.assign(base, '#');
+  std::cout << rectangle << '\n';
+  std::cout << rectangle << '\n';
+  std::cout << rectangle << '\n';
+
+
+
+
+
+
+  return 0;
+}
